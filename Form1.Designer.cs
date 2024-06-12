c1994b8dfb0e84cf6818b3b2827efb1b97edf87e
Skibowy <dawiskibabartosz@gmail.com>
2024-06-12 19:19:08 +0200

    First commit - app works for one element

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace GoldMet_Czas
+{
+	partial class GoldMet
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GoldMet));
+			this.clbListaTowarow = new System.Windows.Forms.CheckedListBox();
+			this.btnOblicz = new System.Windows.Forms.Button();
+			this.lblIlosc = new System.Windows.Forms.Label();
+			this.txtIlosc = new System.Windows.Forms.TextBox();
+			this.lblWieszak = new System.Windows.Forms.Label();
+			this.txtWieszaki = new System.Windows.Forms.TextBox();
+			this.lblIleNaWieszaku = new System.Windows.Forms.Label();
+			this.txtIleNaWieszaku = new System.Windows.Forms.TextBox();
+			this.dtpCzasUkonczenia = new System.Windows.Forms.DateTimePicker();
+			this.lblKosze = new System.Windows.Forms.Label();
+			this.txtKosze = new System.Windows.Forms.TextBox();
+			this.cbElement = new System.Windows.Forms.ComboBox();
+			this.lblElement = new System.Windows.Forms.Label();
+			this.btnZaznaczWszystkie = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// clbListaTowarow
+			// 
+			this.clbListaTowarow.FormattingEnabled = true;
+			this.clbListaTowarow.Location = new System.Drawing.Point(12, 12);
+			this.clbListaTowarow.Name = "clbListaTowarow";
+			this.clbListaTowarow.Size = new System.Drawing.Size(255, 364);
+			this.clbListaTowarow.TabIndex = 0;
+			// 
+			// btnOblicz
+			// 
+			this.btnOblicz.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.btnOblicz.Location = new System.Drawing.Point(450, 380);
+			this.btnOblicz.Name = "btnOblicz";
+			this.btnOblicz.Size = new System.Drawing.Size(147, 56);
+			this.btnOblicz.TabIndex = 1;
+			this.btnOblicz.Text = "Oblicz";
+			this.btnOblicz.UseVisualStyleBackColor = true;
+			this.btnOblicz.Click += new System.EventHandler(this.btnOblicz_Click);
+			// 
+			// lblIlosc
+			// 
+			this.lblIlosc.AutoSize = true;
+			this.lblIlosc.BackColor = System.Drawing.Color.Transparent;
+			this.lblIlosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.lblIlosc.ForeColor = System.Drawing.Color.Gold;
+			this.lblIlosc.Location = new System.Drawing.Point(273, 85);
+			this.lblIlosc.Name = "lblIlosc";
+			this.lblIlosc.Size = new System.Drawing.Size(71, 31);
+			this.lblIlosc.TabIndex = 2;
+			this.lblIlosc.Text = "Ilość";
+			// 
+			// txtIlosc
+			// 
+			this.txtIlosc.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.txtIlosc.Location = new System.Drawing.Point(273, 119);
+			this.txtIlosc.Name = "txtIlosc";
+			this.txtIlosc.Size = new System.Drawing.Size(194, 38);
+			this.txtIlosc.TabIndex = 3;
+			// 
+			// lblWieszak
+			// 
+			this.lblWieszak.AutoSize = true;
+			this.lblWieszak.BackColor = System.Drawing.Color.Transparent;
+			this.lblWieszak.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.lblWieszak.ForeColor = System.Drawing.Color.Gold;
+			this.lblWieszak.Location = new System.Drawing.Point(273, 160);
+			this.lblWieszak.Name = "lblWieszak";
+			this.lblWieszak.Size = new System.Drawing.Size(123, 31);
+			this.lblWieszak.TabIndex = 4;
+			this.lblWieszak.Text = "Wieszaki";
+			// 
+			// txtWieszaki
+			// 
+			this.txtWieszaki.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.txtWieszaki.Location = new System.Drawing.Point(273, 194);
+			this.txtWieszaki.Name = "txtWieszaki";
+			this.txtWieszaki.Size = new System.Drawing.Size(194, 38);
+			this.txtWieszaki.TabIndex = 5;
+			// 
+			// lblIleNaWieszaku
+			// 
+			this.lblIleNaWieszaku.AutoSize = true;
+			this.lblIleNaWieszaku.BackColor = System.Drawing.Color.Transparent;
+			this.lblIleNaWieszaku.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.lblIleNaWieszaku.ForeColor = System.Drawing.Color.Gold;
+			this.lblIleNaWieszaku.Location = new System.Drawing.Point(273, 235);
+			this.lblIleNaWieszaku.Name = "lblIleNaWieszaku";
+			this.lblIleNaWieszaku.Size = new System.Drawing.Size(200, 31);
+			this.lblIleNaWieszaku.TabIndex = 6;
+			this.lblIleNaWieszaku.Text = "Ile na wieszaku";
+			// 
+			// txtIleNaWieszaku
+			// 
+			this.txtIleNaWieszaku.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.txtIleNaWieszaku.Location = new System.Drawing.Point(273, 269);
+			this.txtIleNaWieszaku.Name = "txtIleNaWieszaku";
+			this.txtIleNaWieszaku.Size = new System.Drawing.Size(194, 38);
+			this.txtIleNaWieszaku.TabIndex = 7;
+			// 
+			// dtpCzasUkonczenia
+			// 
+			this.dtpCzasUkonczenia.CalendarMonthBackground = System.Drawing.SystemColors.InactiveBorder;
+			this.dtpCzasUkonczenia.CalendarTitleForeColor = System.Drawing.SystemColors.Desktop;
+			this.dtpCzasUkonczenia.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.dtpCzasUkonczenia.Location = new System.Drawing.Point(279, 336);
+			this.dtpCzasUkonczenia.Name = "dtpCzasUkonczenia";
+			this.dtpCzasUkonczenia.Size = new System.Drawing.Size(509, 38);
+			this.dtpCzasUkonczenia.TabIndex = 8;
+			// 
+			// lblKosze
+			// 
+			this.lblKosze.AutoSize = true;
+			this.lblKosze.BackColor = System.Drawing.Color.Transparent;
+			this.lblKosze.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.lblKosze.ForeColor = System.Drawing.Color.Gold;
+			this.lblKosze.Location = new System.Drawing.Point(557, 235);
+			this.lblKosze.Name = "lblKosze";
+			this.lblKosze.Size = new System.Drawing.Size(90, 31);
+			this.lblKosze.TabIndex = 9;
+			this.lblKosze.Text = "Kosze";
+			// 
+			// txtKosze
+			// 
+			this.txtKosze.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.txtKosze.Location = new System.Drawing.Point(563, 269);
+			this.txtKosze.Name = "txtKosze";
+			this.txtKosze.Size = new System.Drawing.Size(194, 38);
+			this.txtKosze.TabIndex = 10;
+			// 
+			// cbElement
+			// 
+			this.cbElement.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.cbElement.FormattingEnabled = true;
+			this.cbElement.Location = new System.Drawing.Point(273, 43);
+			this.cbElement.Name = "cbElement";
+			this.cbElement.Size = new System.Drawing.Size(515, 39);
+			this.cbElement.TabIndex = 11;
+			// 
+			// lblElement
+			// 
+			this.lblElement.AutoSize = true;
+			this.lblElement.BackColor = System.Drawing.Color.Transparent;
+			this.lblElement.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.lblElement.ForeColor = System.Drawing.Color.Gold;
+			this.lblElement.Location = new System.Drawing.Point(273, 9);
+			this.lblElement.Name = "lblElement";
+			this.lblElement.Size = new System.Drawing.Size(113, 31);
+			this.lblElement.TabIndex = 12;
+			this.lblElement.Text = "Element";
+			// 
+			// btnZaznaczWszystkie
+			// 
+			this.btnZaznaczWszystkie.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.btnZaznaczWszystkie.Location = new System.Drawing.Point(12, 382);
+			this.btnZaznaczWszystkie.Name = "btnZaznaczWszystkie";
+			this.btnZaznaczWszystkie.Size = new System.Drawing.Size(255, 51);
+			this.btnZaznaczWszystkie.TabIndex = 13;
+			this.btnZaznaczWszystkie.Text = "Zaznacz wszystkie";
+			this.btnZaznaczWszystkie.UseVisualStyleBackColor = true;
+			this.btnZaznaczWszystkie.Click += new System.EventHandler(this.btnZaznaczWszystkie_Click);
+			// 
+			// GoldMet
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.WindowFrame;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.ClientSize = new System.Drawing.Size(800, 445);
+			this.Controls.Add(this.btnZaznaczWszystkie);
+			this.Controls.Add(this.lblElement);
+			this.Controls.Add(this.cbElement);
+			this.Controls.Add(this.txtKosze);
+			this.Controls.Add(this.lblKosze);
+			this.Controls.Add(this.dtpCzasUkonczenia);
+			this.Controls.Add(this.txtIleNaWieszaku);
+			this.Controls.Add(this.lblIleNaWieszaku);
+			this.Controls.Add(this.txtWieszaki);
+			this.Controls.Add(this.lblWieszak);
+			this.Controls.Add(this.txtIlosc);
+			this.Controls.Add(this.lblIlosc);
+			this.Controls.Add(this.btnOblicz);
+			this.Controls.Add(this.clbListaTowarow);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "GoldMet";
+			this.ShowIcon = false;
+			this.Text = "GoldMet";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.CheckedListBox clbListaTowarow;
+		private System.Windows.Forms.Button btnOblicz;
+		private System.Windows.Forms.Label lblIlosc;
+		private System.Windows.Forms.TextBox txtIlosc;
+		private System.Windows.Forms.Label lblWieszak;
+		private System.Windows.Forms.TextBox txtWieszaki;
+		private System.Windows.Forms.Label lblIleNaWieszaku;
+		private System.Windows.Forms.TextBox txtIleNaWieszaku;
+		private System.Windows.Forms.DateTimePicker dtpCzasUkonczenia;
+		private System.Windows.Forms.Label lblKosze;
+		private System.Windows.Forms.TextBox txtKosze;
+		private System.Windows.Forms.ComboBox cbElement;
+		private System.Windows.Forms.Label lblElement;
+		private System.Windows.Forms.Button btnZaznaczWszystkie;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace GoldMet_Czas
+{
+	public partial class GoldMet : Form
+	{
+		decimal ilosc = 0;
+		decimal wieszaki = 0;
+		decimal ileNaWieszaku = 0;
+		decimal ileKoszy = 0;
+		string excelFilePath = "C:/Users/dawis/Desktop/a.xls";
+		string txtFilePath = "C:/Users/dawis/Desktop/elementy.txt";
+		List<Element> elements = new List<Element>();
+		List<ElementZExcela> elementyZExcela = new List<ElementZExcela>();
+		Dictionary<string, string> iloscDlaIdentyfikatora = new Dictionary<string, string>();
+		public GoldMet()
+		{
+			InitializeComponent();
+			txtIlosc.Text = ilosc.ToString();
+			txtWieszaki.Text = wieszaki.ToString();
+			txtIleNaWieszaku.Text = ileNaWieszaku.ToString();
+
+			if (File.Exists(excelFilePath))
+			{
+				DataTable dt = LoadExcelData(excelFilePath);
+				for (int i = 0; i < dt.Rows.Count - 1; i++)
+				{
+					DataRow row = dt.Rows[i];
+					if (row.ItemArray.All(field => field == null || string.IsNullOrWhiteSpace(field.ToString())))
+					{
+						continue;
+					}
+					ElementZExcela elementZExcela = new ElementZExcela
+					{
+						Id = int.Parse(row[0].ToString()),
+						Name = row[1].ToString(),
+						Ilosc = int.Parse(row[5].ToString())
+					};
+					elementyZExcela.Add(elementZExcela);
+					clbListaTowarow.Items.Add(row[0].ToString() + " " + row[1].ToString()/* + "  Sztuk: " + row[5].ToString()*/, true);
+					iloscDlaIdentyfikatora.Add(row[0].ToString(), row[5].ToString());
+				}
+			}
+			else
+			{
+				MessageBox.Show("Plik '.xls' nie istnieje");
+			}
+
+			if (File.Exists(txtFilePath))
+			{
+				elements = LoadElementsFromFile(txtFilePath);
+			}
+			else
+			{
+				Console.WriteLine("Plik '.txt' nie istnieje.");
+			}
+
+			this.cbElement.DropDownStyle = ComboBoxStyle.DropDownList;
+			this.cbElement.DataSource = elements;
+			this.cbElement.SelectedIndex = 0;
+			this.cbElement.DisplayMember = "Name";
+			this.cbElement.ValueMember = "Name";
+			this.cbElement.SelectedIndexChanged += new System.EventHandler(this.cbElement_SelectedIndexChanged);
+		}
+
+		private void btnOblicz_Click(object sender, EventArgs e)
+		{
+			ilosc = decimal.Parse(txtIlosc.Text);
+			wieszaki = decimal.Parse(txtWieszaki.Text);
+			ileNaWieszaku = decimal.Parse(txtIleNaWieszaku.Text);
+			if (wieszaki * ileNaWieszaku == 0)
+			{
+				ileKoszy = 0;
+				txtKosze.Text = "0";
+			}
+			else
+			{
+				ileKoszy = (decimal)(ilosc / (wieszaki * ileNaWieszaku));
+			}
+			ileKoszy = Math.Ceiling(ileKoszy);
+
+			foreach (var item in clbListaTowarow.CheckedItems)
+			{
+				string identyfikator = item.ToString().Substring(0, 6);
+				int ilosc = int.Parse(iloscDlaIdentyfikatora[identyfikator]);
+				string nazwaElementu = item.ToString().Substring(7);
+
+				Element znaleziony = elements.FirstOrDefault(x => x.Name == nazwaElementu);
+				wieszaki = znaleziony.IleWieszakow;
+				ileNaWieszaku = znaleziony.IleNaWieszaku;
+
+				if (wieszaki * ileNaWieszaku != 0)
+				{
+					ileKoszy += (decimal)(ilosc / (wieszaki * ileNaWieszaku));
+				}
+			}
+
+			ileKoszy = Math.Ceiling(ileKoszy);
+			txtKosze.Text = ileKoszy.ToString();
+
+			dtpCzasUkonczenia.Value = CalculateCompletionDate((int)ileKoszy);
+		}
+
+		private void btnZaznaczWszystkie_Click(object sender, EventArgs e)
+		{
+			bool allChecked = true;
+
+			for (int i = 0; i < clbListaTowarow.Items.Count; i++)
+			{
+				if (!clbListaTowarow.GetItemChecked(i))
+				{
+					allChecked = false;
+					break;
+				}
+			}
+
+			for (int i = 0; i < clbListaTowarow.Items.Count; i++)
+			{
+				clbListaTowarow.SetItemChecked(i, !allChecked);
+			}
+		}
+
+		private void cbElement_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cbElement.SelectedItem != null)
+			{
+				Element selectedElement = cbElement.SelectedItem as Element;
+				if (selectedElement != null)
+				{
+					wieszaki = (decimal)selectedElement.IleWieszakow;
+					txtWieszaki.Text = wieszaki.ToString();
+					ileNaWieszaku = (decimal)selectedElement.IleNaWieszaku;
+					txtIleNaWieszaku.Text = ileNaWieszaku.ToString();
+				}
+			}
+		}
+
+		private DateTime CalculateCompletionDate(int ileKoszy)
+		{
+			int totalDays = (int)Math.Ceiling(ileKoszy / 10.0);
+			DateTime startDate = DateTime.Today;
+			DateTime completionDate = startDate;
+
+			while (totalDays > 0)
+			{
+				completionDate = completionDate.AddDays(1);
+				if (completionDate.DayOfWeek != DayOfWeek.Saturday && completionDate.DayOfWeek != DayOfWeek.Sunday)
+				{
+					totalDays--;
+				}
+			}
+			return completionDate;
+		}
+
+		private static DataTable LoadExcelData(string path)
+		{
+			System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+
+			using (var stream = File.Open(path, FileMode.Open, FileAccess.Read))
+			{
+				using (var reader = ExcelReaderFactory.CreateReader(stream))
+				{
+					var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+					{
+						ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+						{
+							UseHeaderRow = true
+						}
+					});
+					DataTableCollection tables = result.Tables;
+					DataTable dt = tables[0];
+					return dt;
+				}
+			}
+		}
+
+		private class Element
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+			public int IleWieszakow { get; set; }
+			public int IleNaWieszaku { get; set; }
+		}
+
+		private class ElementZExcela
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+			public int Ilosc { get; set; }
+		}
+
+		private static List<Element> LoadElementsFromFile(string path)
+		{
+			List<Element> elements = new List<Element>();
+
+			foreach (var line in File.ReadLines(path))
+			{
+				string[] parts = line.Split(';');
+				if (parts.Length == 4)
+				{
+					Element element = new Element
+					{
+						Name = parts[1],
+						IleWieszakow = int.Parse(parts[2]),
+						IleNaWieszaku = int.Parse(parts[3])
+					};
+					elements.Add(element);
+				}
+			}
+			return elements;
+		}
+	}
+}
